37e4894faa63963bd4e0af6618aeaa26be52cc6a
Bruno Silva <silva.dev@hotmail.com>
2025-01-09 17:54:58 -0300

    criando rotas dentro do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { Router } from "./Router";
+
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
